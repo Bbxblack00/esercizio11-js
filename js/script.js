bce06340e0a75dce4ed7c40bbcae7dcc1424cc92
Benito <bbxblack@gmail.com>
2021-03-19 12:30:36 +0100

    esercizio live

--- js/script.js
+++ js/script.js
@@ -57,36 +57,118 @@
 
 
 
-function minusClick() {
-    var value = $('#target').text();
-    value--;
-    if (value < 0) {
-        value = 0;
+
+
+// function getRandomValue(min, max) {
+//     var localMin = min;
+//     var localMax = max - min + 1;
+//     return Math.floor(Math.random() * localMax) + localMin;
+// }
+// function minusClick() {
+//     var value1 = $('#target-width').text();
+//     var value2 = $('#target-height').text();
+//     value1--;
+//     value2--;
+//     if (value1 < 0 || value2 < 0) {
+//         value1 = 0;
+//         value2 = 0;
+//         alert('Non fare il furbo!');
+//     }
+//     $('#target').text(value);
+//     var width = parseInt($('#circle').css('width')) ;
+//     console.log(width);
+//     $('#circle').css('width', (width) + 'px');
+// }
+// function plusClick() {
+//     var value = $('#target').text();
+//     value++;
+//     if (value > 20) {
+//         value = 20;
+//         alert('Non fare il furbo!');
+//     }
+//     $('#target').text(value);
+//     $('#circle').css('width', value + 'px');
+// }
+// function init() {
+//     // VALUES
+//     $('#target').text(10);
+//     // BUTTONS
+//     $('#minus').click(minusClick);
+//     $('#plus').click(plusClick);
+//     // CIRCLE
+//     $('#circle').css('width', '100px');
+//     $('#circle').css('height', '100px');
+// }
+// init();
+
+
+
+
+
+
+
+
+
+// ricalcando la soluzione appena vista per la width, generare html + js
+// necessario per ottenere altri 2 tasti - e + che andranno a modificare l'altezza in accordo con il valore centale
+
+
+function minusClickWidth() {
+    var value1 = $('#target-width').text();
+    value1--;
+    if (value1 < 0) {
+        value1 = 0;
         alert('Non fare il furbo!');
-    }
-    $('#target').text(value);
 }
-function plusClick() {
-    var value = $('#target').text();
-    value++;
-    if (value > 20) {
-        value = 20;
+    $('#target-width').text(value1);
+    $('#Circle').css('width', (value1 * 10) + 'px');
+}
+function minusClickHeight() {
+    var value2 = $('#target-height').text();
+    value2--;
+    if (value2 < 0) {
+        value2 = 0;
         alert('Non fare il furbo!');
-    }
-    $('#target').text(value);
-    return value;
 }
+    $('#target-height').text(value2);
+    $('#Circle').css('height', (value2 * 10) + 'px');
+}
+function plusClickWidth() {
+  var value1 = $('#target-width').text();
+  value1++;
+  if (value1 > 20) {
+      value1 = 0;
+      alert('Non fare il furbo!');
+}
+    $('#target-width').text(value1);
+    $('#Circle').css('width', (value1 * 10) + 'px');
+}
+function plusClickHeight() {
+  var value2 = $('#target-height').text();
+  value2++;
+  if (value2 > 20) {
+      value2 = 0;
+      alert('Non fare il furbo!');
+}
+    $('#target-height').text(value2);
+    $('#Circle').css('height', (value2 * 10) + 'px');
+}
+
 function init() {
 
     // VALUES
-    $('#target').text(10);
+    $('#target-width').text(10);
+    $('#target-height').text(10);
     // BUTTONS
-    $('#minus').click(minusClick);
-    $('#plus').click(plusClick);
+    $('#minus-width').click(minusClickWidth);
+    $('#plus-width').click(plusClickWidth);
+    $('#minus-height').click(minusClickHeight);
+    $('#plus-height').click(plusClickHeight);
     // VARIABILI
-    var val = $('#target').val();
+    var value1 = $('#target-Width').html();
+    var value2 = $('#target-Height').html();
     // CIRCLE
-    $('#circle').css('width', val + 'px');
-    $('#circle').css('height', val + 'px');
+    $('#Circle').css('width', value1 + 'px');
+    $('#Circle').css('height', value2 + 'px');
 }
 init();
